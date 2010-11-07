1e8e6cd5ead83087302f670ef44e10abce3c66ea
Tim <trliner@gmail.com>
2010-11-06 21:41:31 -0500

    added support for block selectors

--- examples/google.rb
+++ examples/google.rb
@@ -1,6 +1,11 @@
 require '../client/nibblet_client.rb'
 
 selectors = {}
-selectors[:single] = {:results => "div#ires"}
+selectors[:block] = {
+  :results => {
+    :selector => "div#ires li",
+    :block => {:title => "h3.r a", :url => "h3.r a/@href"}
+  }
+}
 google = NibbletClient.new("http://www.google.com/search?q=ruby+resources", selectors)
 puts google.scrape
--- server/nibblet.rb
+++ server/nibblet.rb
@@ -15,18 +15,41 @@ class Nibblet < Nibbler
     selector_hash["multiple"].each do |element_key, selector|
       self.elements selector => element_key.to_sym
     end
+    selector_hash["block"].each do |block_key, opts|
+      self.elements opts["selector"] => block_key.to_sym do
+        opts["block"].each do |element_key, selector|
+          self.element selector => element_key.to_sym
+        end
+      end
+    end
   end
 
   def output_results(selector_hash)
     results = {}
     results[:single] = self.collect_elements(selector_hash["single"].keys)
     results[:multiple] = self.collect_elements(selector_hash["multiple"].keys)
+    results[:block] = self.collect_iterators(selector_hash["block"])
     results.to_json
   end
 
   def collect_elements(element_keys)
     element_keys.collect{ |key| {key => self.send(key.to_sym)} }
   end
+
+  def collect_iterators(selector_type)
+    selector_type.keys.collect do |b_key|
+      block_keys = selector_type[b_key.to_s]["block"].keys
+      block_array = self.send(b_key.to_sym).inject([]) do |array, iterator|
+        element_hash = block_keys.inject({}) do |hash, e_key|
+          element = iterator.send(e_key.to_sym)
+          hash.merge(e_key => element)
+        end
+        array << element_hash
+      end
+      {b_key => block_array}
+    end
+  end
+
 end
 
 get '/nibblet' do
